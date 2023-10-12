387e588eae9e75981f59c1208e1f61c8aaa06eea
unknown <deon.vx@gmail.com>
2023-10-12 17:43:58 +0700

    revert

--- frontend/src/app/containers/App/index.tsx
+++ frontend/src/app/containers/App/index.tsx
@@ -1,36 +1,59 @@
-import React from 'react';
-import { Route, Switch, Redirect, RouteComponentProps } from 'react-router';
-import App from './containers/App';
-import { hot } from 'react-hot-loader';
-import './style.local.css';
+import * as React from 'react';
+import { Component } from 'react';
+import { RouteComponentProps, Switch, Route, Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import { Navbar } from 'app/components/Navbar';
+import AddEmployee from 'app/containers/Employee/components/AddEmployee';
+import EditEmployee from 'app/containers/Employee/components/EditEmployee';
+import Employees from 'app/containers/Employee/components/Employees';
+import 'react-toastify/dist/ReactToastify.css';
+import '../../style.local.css';
+import style from '../../style.local.css';
+import { ToastContainer } from 'react-toastify';
+import ViewEmployee from 'app/containers/Employee/components/ViewEmployee';
 
-export interface Props {}
-
-export interface State {
-  showLoader: boolean;
+export namespace App {
+  export interface Props extends RouteComponentProps<void> {}
 }
-export class Root extends React.Component<Props, State> {
-  state = {
-    showLoader: true,
-  };
 
-  componentDidMount() {
-    setTimeout(() => this.setState({ showLoader: false }), 1000);
+class App extends Component<App.Props> {
+  constructor(props: App.Props) {
+    super(props);
   }
+
   render() {
+    const autoCloseTime = process.env.TOASTER_AUTO_CLOSE_TIME_IN_MILLISECONDS
+      ? parseInt(process.env.TOASTER_AUTO_CLOSE_TIME_IN_MILLISECONDS, 10)
+      : 3000;
+
+    const CloseButton = ({ closeToast }: any) => (
+      <a href="javascript:void(0)" className={style['e-toaster-close']}>
+        <i
+          className={`${style.icon} ${style['i-cancel']} ${style['margin-left']}`}
+          aria-hidden="true"
+          onClick={closeToast}
+        />
+      </a>
+    );
+
     return (
       <div>
+        <Navbar history={this.props.history} />
         <Switch>
-          <Route exact path="/" render={() => <Redirect to="/employees" />} />
-          <Route
-            render={(props: RouteComponentProps<void>) => {
-              return <App {...props} />;
-            }}
-          />
+          <Route exact path="/employees" component={Employees} />
+          <Route exact path="/employees/new" component={AddEmployee} />
+          <Route path="/employees/:employeeId/edit" component={EditEmployee} />
+          <Route path="/employees/:employeeId/view" component={ViewEmployee} />
+          <Route render={() => <Redirect to="/error" />} />
         </Switch>
+        <ToastContainer
+          closeButton={<CloseButton />}
+          autoClose={autoCloseTime}
+          hideProgressBar={true}
+        />
       </div>
     );
   }
 }
 
-export default hot(module)(Root);
+export default connect()(App);
